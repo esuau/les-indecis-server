83a41ae4523008c375689fce5402a9e41ee7f899
Hugo STEPHAN <hugostephanbac@gmail.com>
2018-11-22 00:41:37 +0100

    Ajout d'une connexion à la VIP postgresql

--- app/index.js
+++ app/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
+const pg = require('pg');
+const pool = new pg.Pool({
+user: ‘postgres’,
+host: ‘bdd-vip.undefined.inside.esiag.info’,
+database: ‘pds’,
+password: ‘undefined’,
+port: ‘5432’});
+
 const app = express();
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/heartbeat.json')
 });
+
+app.get('/get_notifications', (req, res) => {
+	pool.query(“SELECT NOW()”, (err, res) => {
+		console.log(res);
+		pool.end();
+	});
+});
+
+
 
 var listener = app.listen(process.env.PORT || 80, function() {
  console.log('listening on port ' + listener.address().port);

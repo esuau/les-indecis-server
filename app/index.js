0846ba8d8297d1e32b21507a33535c00bdd2e9ef
Hugo STEPHAN <hugo.stephan@etu.u-pec.fr>
2018-12-07 17:30:27 +0100

    Conversion Buffer to string

--- app/index.js
+++ app/index.js
@@ -118,6 +118,7 @@ cron.schedule('* * * * *', () => {
 	request('http://api.undefined.inside.esiag.info/get_msg?queue=test', { json: false }, (err, res, body) => {
 		if (err) { return console.log(err); }
 		console.log(body);
+		var msg = body.content.data.toString();
 		pool.query("SELECT MAX(ID) AS mid FROM notification;", (err, r) => {
 		if(err) { res.send("Error while reading id from DB : " + err); }
 		else 

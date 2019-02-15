75ac29afe80764b9e30a49f810556a9eb924157d
esuau <evan.suau@etu.u-pec.fr>
2019-02-15 13:32:53 +0100

    feat(invoice): Get data from database

--- app/index.js
+++ app/index.js
@@ -81,35 +81,59 @@ app.get('/get_spots', (req, res) => {
 });
 
 // Request to trigger the generation of invoices.
-app.post('/generate_invoice', (req, res) => {
-	customer = req.body;
-	pdfInvoice.lang = 'en_US';
-	const document = pdfInvoice({
-		company: {
-			phone: '(99) 9 9999-9999',
-			email: 'contact@undefined.com',
-			address: '71, Rue Saint Simon, 94000 Créteil',
-			name: 'Les Indécis',
-		},
-		customer: {
-			name: customer.name,
-			email: customer.email,
-		},
-		items: customer.items
+app.get('/generate_invoice', (req, res) => {
+	paths = [];
+	pool.query(`SELECT b.bill_id, a.firstname, a.lastname, a.email, b.totalprice, h.vehicle, h.description
+			    FROM bill b, account a, historic h
+	            LEFT JOIN account a2 on h.account_id = a2.user_id
+	            LEFT JOIN bill b2 on h.historic_id = b2.historic_id
+	            WHERE b.notification_status = 'pending';`, (err, r) => {
+		if(err) {
+			res.send(500, "Error while reading notifications from DB : " + err);
+		} else {
+			r.rows.forEach(element => {
+				pdfInvoice.lang = 'en_US';
+				const document = pdfInvoice({
+					company: {
+						phone: '(99) 9 9999-9999',
+						email: 'contact@undefined.com',
+						address: '71, Rue Saint Simon, 94000 Créteil',
+						name: 'Les Indécis',
+					},
+					customer: {
+						name: element.firstname + ' ' + element.lastname,
+						email: element.email
+					},
+					items: [{
+						amount: element.totalprice,
+						name: element.vehicle,
+						description: element.description
+					}]
+				});
+				document.generate();
+				now = new Date();
+				fileName = '/usr/shared/bill'
+					+ now.getFullYear()
+					+ (now.getMonth() + 1)
+					+ now.getDate()
+					+ now.getHours()
+					+ now.getMinutes()
+					+ now.getSeconds()
+					+ now.getMilliseconds()
+					+ '.pdf';
+				document.pdfkitDoc.pipe(fs.createWriteStream(fileName));
+				console.log(element);
+				console.log(fileName);
+				paths.push({ path: fileName });
+				pool.query("UPDATE bill SET notification_status = 'created' WHERE bill_id = " + element.bill_id + " ;", (eerr, u) => {
+					if (eerr) {
+						res.status(500).send("Could not update notification status: " + eerr);
+					}
+				});
+			});
+			res.status(200).send({ paths: paths });
+		}
 	});
-	document.generate();
-	now = new Date();
-	fileName = '/usr/shared/bill/'
-		+ now.getFullYear()
-		+ (now.getMonth() + 1)
-		+ now.getDate()
-		+ now.getHours()
-		+ now.getMinutes()
-		+ now.getSeconds()
-		+ now.getMilliseconds()
-		+ '.pdf';
-	document.pdfkitDoc.pipe(fs.createWriteStream(fileName));
-	res.send(200, { path: fileName });
 });
 
 var listener = app.listen(process.env.PORT || 80, function() {

a431e4f0a11611528d41fabcda41334d4ced88a4
esuau <evan.suau@etu.u-pec.fr>
2019-05-21 21:35:15 +0200

    refactor(cron): Move cron jobs form app to www

--- app.js
+++ app.js
@@ -4,20 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const cron = require('node-cron');
-const request = require('request');
-
-const helper = require('./util/helper');
-const config = helper.readConfig('./conf/config.json');
-const pg = require('pg');
-const pool = new pg.Pool({
-  user: config.psql.user,
-  host: config.psql.host,
-  database: config.psql.database,
-  password: config.psql.password,
-  port: config.psql.port
-});
-
 var heartbeatRouter = require('./routes/heartbeat');
 var getNotificationsRouter = require('./routes/get-notifications');
 var addNotificationsRouter = require('./routes/add-notifications');
@@ -41,46 +27,6 @@ app.use('/get_spots', spotRouter);
 app.use('/get_macs', macAddressRouter);
 app.use('/get_vehicles', vehiclesRouter);
 app.use('/generate_invoice', invoicesRouter);
-
-cron.schedule('* * * * *', () => {
-  request('http://api.undefined.inside.esiag.info/get_msg?queue=test', { json: false }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body);
-    if (body.content != undefined) {
-      var msg = body.content.data.toString();
-      pool.query("SELECT MAX(ID) AS mid FROM notification;", (err, r) => {
-        if (err) {
-          res.status(500).json({message: 'Error while reading id from DB : ' + err});
-        } else {
-          var id = r.rows[0].mid + 1;
-          pool.query("INSERT INTO notification VALUES (" + id + ", '" + msg + "', NOW(), '" + planned_at + "', '##') ;", (err, r) => {
-            if (err) {
-              res.status(500).json({message: 'Error while adding notification in DB : ' + err});
-            } else {
-              res.send('Notification ajoutée avec succés !');
-            }
-          });
-        }
-      });
-    };
-  });
-});
-
-cron.schedule('* * * * *', () => {
-  var today = new Date(Date.now()).toLocaleString();
-  while (today.indexOf('/') != -1) {
-    today = today.replace('/', '-');
-  }
-  today = today.split(' ')[0];
-  var tab = today.split('-');
-  today = '' + tab[2] + '-' + tab[1] + '-' + tab[0];
-
-  pool.query("SELECT * FROM notification WHERE planned_at::text LIKE '" + today + "%' ;", (err, r) => {
-    if (err) {
-      console.error('Error while reading id from DB : ' + err);
-    }
-  });
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

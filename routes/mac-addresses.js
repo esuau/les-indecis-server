0668a2a9eef192342eaf8226b3cb01a271d1fe05
esuau <evan.suau@etu.u-pec.fr>
2019-05-21 21:36:07 +0200

    test(mac-addresses): Add unit test for mac addresses

--- routes/mac-addresses.js
+++ routes/mac-addresses.js
@@ -12,8 +12,8 @@ const pool = new pg.Pool({
   port: config.psql.port
 });
 
-/* GET heartbeat. */
-router.post('/', function (req, res, next) {
+/* GET mac addresses. */
+router.get('/', function (req, res, next) {
   var msg = decodeURIComponent(req.body.msg);
   var planned_at = decodeURIComponent(req.body.planned);
 
@@ -22,7 +22,7 @@ router.post('/', function (req, res, next) {
       res.status(500).json({message: 'Error while reading id from DB : ' + err});
     } else {
       var id = r.rows[0].mid + 1;
-      pool.query("INSERT INTO notification VALUE (" + id + ", '" + msg + "', NOW(), '" + planned_at + "') ;", (err, r) => {
+      pool.query("INSERT INTO notification VALUES (" + id + ", '" + msg + "', NOW(), NOW()) ;", (err, r) => {
         if (err) {
           res.status(500).json({message: 'Error while adding notification in DB : ' + err});
         } else {
---
+++ test/mac-addresses.spec.js
@@ -0,0 +1,22 @@
+var chai = require('chai')
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+var app = require('../app')
+
+chai.use(chaiHttp);
+
+describe('Mac addresses', () => {
+  it('should return mac addresses correctly', function (done) {
+    chai.request(app)
+      .get('/get_macs')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // var l = JSON.parse(res.text);
+        // l.forEach(function (e) {
+        //   assert(e.address != undefined);
+        // });
+        expect(res.text).to.equal('Notification ajoutée avec succès !');
+        done();
+      });
+  });
+});
